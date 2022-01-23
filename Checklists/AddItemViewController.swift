245f4894c951dbb01251176eab9ca95aaebf4d05
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-01-23 04:56:37 -0500

    Cancel and done buttons functionality

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Tyler Bischoff on 1/23/22.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    // MARK: - Actions
+    @IBAction func cancel(){
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func done(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
