585ac02103a65c08d2463e4354962edb65b62a44
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-01-23 02:26:14 -0500

    start of second iteration

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Tyler Bischoff on 1/23/22.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
