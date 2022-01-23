279f5c79afadafa3e7022ce4ca1a8f8e24c7ab41
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-01-23 03:13:42 -0500

    clean code further added new config text method

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -54,10 +54,8 @@ class ChecklistViewController: UITableViewController {
         
         let item = items[indexPath.row]
         
-        let label = cell.viewWithTag(1000) as! UILabel
-        
-        label.text = item.text
-        configureCheckmark(for: cell, at: indexPath)
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
         
         return cell
     }
@@ -69,15 +67,14 @@ class ChecklistViewController: UITableViewController {
     ){
         if let cell = tableView.cellForRow(at: indexPath) {
             let item = items[indexPath.row]
+            
             item.checked.toggle()
-        
-            configureCheckmark(for: cell, at: indexPath)
+            configureCheckmark(for: cell, with: item)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath){
-        let item = items[indexPath.row]
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem){
         
         if item.checked {
             cell.accessoryType = .checkmark
@@ -85,5 +82,10 @@ class ChecklistViewController: UITableViewController {
         else {
             cell.accessoryType = .none
         }
+    }
+    
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem){
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
     }
 }
